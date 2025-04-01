ff42a55009168b205ec4012ce8cfb7e03dd1c478
Juan Pablo Sarmiento <pablowritescode@gmail.com>
2025-04-01 09:50:35 -0400

    Added comments

--- src/types/mantine.d.ts
+++ src/types/mantine.d.ts
@@ -3,10 +3,19 @@ import "@mantine/core";
 declare module "@mantine/core" {
   export interface MantineThemeOther {
     navbar: {
+      /** Navbar background color */
       backgroundColor: string;
+
+      /** Navbar text color */
       textColor: string;
+
+      /** Navbar link background color on hover */
       hoverBackgroundColor: string;
+
+      /** Active navbar link background color*/
       activeBackgroundColor: string;
+
+      /** Active navbar link background color on hover */
       activeHoverBackgroundColor: string;
     };
   }
