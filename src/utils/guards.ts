f06eab8e045e9870fbed58b593a31c40d4de72ab
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2025-04-08 23:30:21 -0400

    Added helper functions

---
+++ src/utils/guards.ts
@@ -0,0 +1,65 @@
+import { UnknownObject } from "@/types/common";
+
+/**
+ * Inspired from Remeda's `isPlainObject`.
+ * Checks if `value` is a plain object, i.e. an object with string
+ * keys and values. This will not consider other entities JavaScript
+ * considers an object using `typeof` (like Arrays, Maps, Sets, etc.).
+ *
+ * We're talking about just plain old objects defined with curly braces {}.
+ *
+ * **Examples**
+ *
+ * ```ts
+ * isPlainObject({}); // true
+ * isPlainObject({ foo: 'bar' }); // true
+ * isPlainObject([]) // false
+ * isPlainObject("foo"); // false
+ * isPlainObject(new Date()) // false
+ * isPlainObject(null) // false
+ * ```
+ */
+export function isPlainObject<T>(
+  value: Readonly<UnknownObject> | T,
+): value is T & UnknownObject {
+  if (typeof value !== "object" || value === null) {
+    return false;
+  }
+
+  // Inspired from Remeda's `isPlainObject` - this is a low-level check
+  const proto = Object.getPrototypeOf(value);
+  return proto === null || proto === Object.prototype;
+}
+
+/**
+ * Checks if `value` is a `Date` instance.
+ *
+ * **Examples**
+ *
+ * ```ts
+ * isDate(new Date()); // true
+ * isDate("2023-01-01"); // false
+ * isDate(null); // false
+ * isDate(undefined); // false
+ * ```
+ */
+export function isDate(value: unknown): value is Date {
+  return value instanceof Date;
+}
+
+/**
+ * Checks if `value` is not `null` or `undefined`.
+ *
+ * **Examples**
+ *
+ * ```ts
+ * isNotNullOrUndefined(null); // false
+ * isNotNullOrUndefined(undefined); // false
+ * isNotNullOrUndefined("foo"); // true
+ * isNotNullOrUndefined(0); // true
+ * isNotNullOrUndefined(false); // true
+ * ```
+ */
+export function isNotNullOrUndefined<T>(value: T): value is NonNullable<T> {
+  return value !== null && value !== undefined;
+}
