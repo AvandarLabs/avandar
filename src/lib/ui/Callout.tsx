043d26c3750a76159b9fb2cc85d07eaea73b34f9
Juan Pablo Sarmiento <pablowritescode@gmail.com>
2025-04-09 23:24:57 -0400

    Updated callout

--- src/lib/ui/Callout.tsx
+++ src/lib/ui/Callout.tsx
@@ -1,22 +1,41 @@
-import { Alert, AlertProps, Title } from "@mantine/core";
+import { Alert, AlertProps, Stack, Text, Title } from "@mantine/core";
 import { IconAlertCircle } from "@tabler/icons-react";
 
 type Props = {
   title: string;
+  /**
+   * If a message is provided, it will be displayed in a Text component.
+   * If children are provided, they will be displaced under the message.
+   */
+  message?: string;
 } & AlertProps;
 
 export function Callout({
   title,
+  message,
   color = "danger",
-  ...alertProps
+  variant = "light",
+  icon = <IconAlertCircle />,
+  children,
+  ...moreAlertProps
 }: Props): JSX.Element {
+  const contents = (
+    <Stack>
+      {message ?
+        <Text>{message}</Text>
+      : null}
+      {children}
+    </Stack>
+  );
+
   return (
     <Alert
-      variant="light"
       color={color}
+      variant={variant}
       title={<Title order={4}>{title}</Title>}
-      icon={<IconAlertCircle />}
-      {...alertProps}
+      icon={icon}
+      children={contents}
+      {...moreAlertProps}
     />
   );
 }
