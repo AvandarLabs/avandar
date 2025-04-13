ca7967c9c7b330a2745bfc0c17feedf8a0e8b2b1
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2025-04-12 23:11:23 -0400

    Added type to the supabase client

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "@/types/database.types";
 
-export const SupabaseClient = createClient(
+export const SupabaseClient = createClient<Database>(
   import.meta.env.VITE_SUPABASE_API_URL ?? "",
   import.meta.env.VITE_SUPABASE_ANON_KEY ?? "",
 );
