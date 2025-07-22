09a6e3881e813314df39f5dcce6622b7302d78f8
Demian Sims <demian@avandarlabs.com>
2025-07-22 16:51:42 -0400

    added a table with name and role

---
+++ src/hooks/workspaces/useWorkspaceRole.ts
@@ -0,0 +1,48 @@
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import { useCurrentWorkspace } from "@/hooks/workspaces/useCurrentWorkspace";
+import { SupabaseDBClient } from "@/lib/clients/supabase/SupabaseDBClient";
+import { useAuth } from "@/lib/hooks/auth/useAuth";
+import { routeTree } from "@/routeTree.gen";
+
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: {
+    user: undefined,
+    queryClient,
+  },
+  defaultPreload: "intent",
+  scrollRestoration: true,
+});
+
+export function useWorkspaceRole() {
+  const { user } = useAuth(router);
+  const workspace = useCurrentWorkspace();
+
+  const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!user?.id || !workspace?.id) return;
+
+    const fetchRole = async () => {
+      const { data, error } = await SupabaseDBClient.from("user_roles")
+        .select("role")
+        .eq("user_id", user.id)
+        .eq("workspace_id", workspace.id)
+        .single();
+
+      if (data?.role) {
+        setRole(data.role);
+      } else {
+        console.error("Failed to get role:", error);
+      }
+    };
+
+    fetchRole();
+  }, [user?.id, workspace?.id]);
+
+  return role;
+}
--- src/lib/ui/forms/WorkspaceUsersForm.tsx
+++ src/lib/ui/forms/WorkspaceUsersForm.tsx
@@ -1,4 +1,6 @@
-import { Card, Container, Table } from "@mantine/core";
+import { Box, Button, Card, Flex, Group, Table, Text } from "@mantine/core";
+import { IconPencil, IconTrash } from "@tabler/icons-react";
+import { useWorkspaceRole } from "@/hooks/workspaces/useWorkspaceRole";
 
 const userTable = [
   { name: "Jane Doe", role: "Admin" },
@@ -7,28 +9,47 @@ const userTable = [
 ];
 
 export function WorkspaceUserForm(): JSX.Element {
+  const workspaceRole = useWorkspaceRole();
+
+  const isAdmin = workspaceRole === "admin";
+
   const users: any = userTable.map((user) => {
     return (
       <Table.Tr key={user.name}>
         <Table.Td>{user.name}</Table.Td>
         <Table.Td>{user.role}</Table.Td>
+        {isAdmin && (
+          <Table.Td>
+            <Group gap="xs" justify="flex-end">
+              <IconPencil size={16} />
+              <IconTrash size={16} />
+            </Group>
+          </Table.Td>
+        )}
       </Table.Tr>
     );
   });
 
   return (
-    <Container>
-      <Card withBorder mt="md" p="lg">
+    <Box w="100%" px="lg">
+      <Card withBorder mt="md" p="lg" w="100%" maw="1000px">
+        <Flex justify="space-between" align="center" mb="md">
+          <Text>Workspace Users</Text>
+          <Button>Add User</Button>
+        </Flex>
         <Table>
           <Table.Thead>
             <Table.Tr>
-              <Table.Th>Name</Table.Th>
-              <Table.Th>Role</Table.Th>
+              <Table.Th w="300px">
+                <Box pr="lg">Name</Box>
+              </Table.Th>
+              <Table.Th w="600px">Role</Table.Th>
+              <Table.Th w="200x">Action</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>{users}</Table.Tbody>
         </Table>
       </Card>
-    </Container>
+    </Box>
   );
 }
