403a2ade53940548b64217fa2caf8f3b94b1d417
Demian Sims <demian@avandarlabs.com>
2025-08-19 11:28:25 -0400

    refactored chart types

--- src/lib/ui/data-viz/ChartTypes.ts
+++ src/lib/ui/data-viz/ChartTypes.ts
@@ -1,10 +1,15 @@
+<<<<<<< HEAD
 import { UnknownDataFrame } from "@/lib/types/common";
+=======
+import type { UnknownDataFrame } from "@/lib/types/common";
+>>>>>>> e11f28c (refactored chart types)
 
 export type XYSettings = {
   xAxisKey: string;
   yAxisKey: string;
 };
 
+<<<<<<< HEAD
 /** Common props for charts with X and Y axes. */
 export type XYChartProps = {
   /**
@@ -16,3 +21,20 @@ export type XYChartProps = {
   yAxisKey: string;
   height: number;
 };
+=======
+export type XYChartProps = {
+  /**
+   * The `data` structure is a conventional dataframe (an array of objects), but
+   * semantically the interpretation is different. Each object represents a
+   * collection of bars for one bucket.
+   *
+   * One key in the object should be the `dataKey` which is used to bucket
+   * the data. The remaining keys are the names of the series.
+   * The value of each series should be a number.
+   */
+  data: UnknownDataFrame;
+  height?: number;
+} & Required<Pick<XYSettings, "xAxisKey" | "yAxisKey">>;
+
+export type ChartType = "bar" | "line";
+>>>>>>> e11f28c (refactored chart types)
--- src/lib/ui/data-viz/LineChart.tsx
+++ src/lib/ui/data-viz/LineChart.tsx
@@ -1,20 +1,13 @@
 import { LineChart as MantineLineChart } from "@mantine/charts";
 import { useMemo } from "react";
-import { UnknownDataFrame } from "@/lib/types/common";
-
-type Props = {
-  data: UnknownDataFrame;
-  height?: number;
-  xAxisKey: string; // bucket key
-  yAxisKey: string; // must be numeric
-};
+import { XYChartProps } from "./ChartTypes";
 
 export function LineChart({
   data,
   xAxisKey,
   yAxisKey,
   height = 500,
-}: Props): JSX.Element {
+}: XYChartProps): JSX.Element {
   const series = useMemo(() => {
     return [{ name: yAxisKey }];
   }, [yAxisKey]);
@@ -25,10 +18,6 @@ export function LineChart({
       data={data}
       dataKey={xAxisKey}
       series={series}
-      // optional Mantine props:
-      // curveType="linear"  // or "monotone"
-      // withLegend={false}
-      // withDots={false}
     />
   );
 }
