b526f1e0b5590367ba11f34eb182ad4cb6f71e91
Demian Sims <demian@avandarlabs.com>
2025-09-05 14:08:38 -0400

    accepted all PR suggestions

--- src/lib/ui/data-viz/ChartTypes.ts
+++ src/lib/ui/data-viz/ChartTypes.ts
@@ -1,29 +1,14 @@
-import type { UnknownDataFrame } from "@/lib/types/common";
-
 export type XYSettings = {
-  /**
-   * The data object key used to bucket the X-axis (categories or time).
-   * Must correspond to a string or date-like field in the dataset.
-   */
   xAxisKey: string;
-  /**
-   * The data object key used for Y-axis numeric values.
-   * Must map to a number for each row in the dataframe.
-   */
   yAxisKey: string;
 };
 
+/** Common props for charts with X and Y axes. */
 export type XYChartProps = {
   /**
    * The `data` structure is a conventional dataframe (an array of objects).
-   * Each object represents a single row (or bucket).
-   *
-   * One key in the object should be the `dataKey`, which is used to bucket
-   * the data. The remaining keys are treated as series names, and their values
-   * must be numeric.
+   * Each object's keys can be referenced by `xAxisKey` and `yAxisKey`.
    */
-  data: UnknownDataFrame;
-  height?: number;
-} & Required<Pick<XYSettings, "xAxisKey" | "yAxisKey">>;
-
-export type ChartType = "bar" | "line";
+  data: Array<Record<string, unknown>>;
+  settings: XYSettings;
+};
