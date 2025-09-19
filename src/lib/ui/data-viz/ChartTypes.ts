493f90e0b99b4930674b31ebb300515c41d694b6
Demian Sims <demian@avandarlabs.com>
2025-09-19 14:14:12 -0400

    fixed type error

--- src/lib/ui/data-viz/ChartTypes.ts
+++ src/lib/ui/data-viz/ChartTypes.ts
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { UnknownDataFrame } from "@/lib/types/common";
-=======
-import type { UnknownDataFrame } from "@/lib/types/common";
->>>>>>> e11f28c (refactored chart types)
-
-=======
->>>>>>> b526f1e (accepted all PR suggestions)
-=======
 import { UnknownDataFrame } from "@/lib/types/common";
 
->>>>>>> 212b3a4 (fixed more type issues)
 export type XYSettings = {
   xAxisKey: string;
   yAxisKey: string;
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 /** Common props for charts with X and Y axes. */
 export type XYChartProps = {
   /**
@@ -29,28 +15,4 @@ export type XYChartProps = {
   xAxisKey: string;
   yAxisKey: string;
   height: number;
-<<<<<<< HEAD
 };
-=======
-=======
-/** Common props for charts with X and Y axes. */
->>>>>>> b526f1e (accepted all PR suggestions)
-export type XYChartProps = {
-  /**
-   * The `data` structure is a conventional dataframe (an array of objects).
-   * Each object's keys can be referenced by `xAxisKey` and `yAxisKey`.
-   */
-<<<<<<< HEAD
-  data: UnknownDataFrame;
-  height?: number;
-} & Required<Pick<XYSettings, "xAxisKey" | "yAxisKey">>;
-
-export type ChartType = "bar" | "line";
->>>>>>> e11f28c (refactored chart types)
-=======
-  data: Array<Record<string, unknown>>;
-  settings: XYSettings;
-=======
->>>>>>> 212b3a4 (fixed more type issues)
-};
->>>>>>> b526f1e (accepted all PR suggestions)
