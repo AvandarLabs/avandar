4e4e5424dc56b31a26238c84e7845afaf76d051b
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2025-09-29 18:22:52 +0200

    removed unnecessary migrations

--- supabase/migrations/20250922093535_Added unique constraint to entities table.sql
+++
@@ -1,5 +0,0 @@
-CREATE UNIQUE INDEX entities__entity_config_external_id_unique ON public.entities USING btree (entity_config_id, external_id);
-
-alter table "public"."entities" add constraint "entities__entity_config_external_id_unique" UNIQUE using index "entities__entity_config_external_id_unique";
-
-
