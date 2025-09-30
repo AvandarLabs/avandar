b3745612085b2029f4ce648391701601ade65da8
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2025-09-30 12:47:03 +0200

    Added a comment

--- src/lib/utils/objects/builders.ts
+++ src/lib/utils/objects/builders.ts
@@ -74,6 +74,11 @@ export function makeObject<
 
 /**
  * Creates an object from a list of [key, value] tuples.
+ *
+ * This function has better type inference than `Object.fromEntries` because it
+ * correctly creates a record with `K` as the key type, rather than always
+ * enforcing `string`.
+ *
  * @param entries The list of [key, value] tuples to convert.
  * @returns An object with keys and values extracted from the entries.
  */
