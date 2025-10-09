56917a56c6bebe47c2717c8605136cb88b0eb882
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2025-10-09 19:33:28 -0400

    Adding sensible defaults to queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,31 @@ import { routeTree } from "./routeTree.gen";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Minimize surprise refetching and heavy local work (with DuckDB)
+      // Queries that can benefit from refetching should be explicitly set.
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+
+      // Only fetch on mount if the query is stale or not in the cache
+      refetchOnMount: true,
+
+      // Performance/cache
+      staleTime: 6 * 60 * 1000, // 5 minutes
+      gcTime: 30 * 60 * 1000, // 30 minutes
+
+      // Avoids duplicate error toasts from retries in dev
+      retry: 0,
+    },
+    mutations: {
+      // Many mutations are not idempotent and so we should not automatically
+      // retry them. Retries should be explicit.
+      retry: 0,
+    },
+  },
+});
 
 const router = createRouter({
   routeTree,
