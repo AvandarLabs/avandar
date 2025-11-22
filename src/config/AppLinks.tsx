e121c7aa88b894e5592fd7eacf2d4c2002d04603
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2025-11-22 10:26:50 -0500

    minor label change

--- src/config/AppLinks.tsx
+++ src/config/AppLinks.tsx
@@ -43,7 +43,7 @@ export const AppLinks = {
       key: "workspace-home",
       to: "/$workspaceSlug",
       params: { workspaceSlug },
-      label: "Workspace Home",
+      label: "Home",
     };
   },
 
