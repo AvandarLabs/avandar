4cb42f124b133c59fe193d70b50493166535aa32
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2026-01-15 19:27:25 -0500

    Added puck and fixed objectToPrettyString

--- src/views/DashboardApp/DashboardEditorView/DashboardEditorView.tsx
+++ src/views/DashboardApp/DashboardEditorView/DashboardEditorView.tsx
@@ -1,12 +1,54 @@
 import { Center, Stack, Text, Title } from "@mantine/core";
+import { Puck } from "@puckeditor/core";
+import { notifyDevAlert } from "@/lib/ui/notifications/notifyDevAlert";
 import { Paper } from "@/lib/ui/Paper";
 import type { DashboardRead } from "@/models/Dashboard/Dashboard.types";
+import type { Config, Data } from "@puckeditor/core";
+import "@puckeditor/core/puck.css";
+import { useState } from "react";
 
 type Props = {
   readonly dashboard: DashboardRead | undefined;
 };
 
+type HeadingBlockProps = {
+  children: string;
+};
+
+type DashboardPuckData = Data<{
+  HeadingBlock: HeadingBlockProps;
+}>;
+
+const PuckConfig: Config<{
+  HeadingBlock: HeadingBlockProps;
+}> = {
+  components: {
+    HeadingBlock: {
+      fields: {
+        children: {
+          type: "text",
+        },
+      },
+      render: ({ children }: HeadingBlockProps) => {
+        return <h1>{children}</h1>;
+      },
+    },
+  },
+};
+
+const EMPTY_DATA: DashboardPuckData = {
+  root: { props: {} },
+  content: [],
+};
+
+function onPublish(data: DashboardPuckData): void {
+  console.log("onPublish", data);
+  notifyDevAlert(data);
+}
+
 export function DashboardEditorView({ dashboard }: Props): JSX.Element {
+  const [data, setData] = useState<DashboardPuckData>(EMPTY_DATA);
+
   return (
     <Stack gap="lg">
       <Paper>
@@ -28,6 +70,13 @@ export function DashboardEditorView({ dashboard }: Props): JSX.Element {
           </Stack>
         </Center>
       </Paper>
+
+      <Puck
+        config={PuckConfig}
+        data={data}
+        onChange={setData}
+        onPublish={onPublish}
+      />
     </Stack>
   );
 }
