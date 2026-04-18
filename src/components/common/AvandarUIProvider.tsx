7fe044cd202d9b95fe653641ba1a5aa56b5a0f13
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2026-04-18 18:57:22 -0400

    fixed lint errors and tests

--- src/components/common/AvandarUIProvider.tsx
+++ src/components/common/AvandarUIProvider.tsx
@@ -2,8 +2,6 @@ import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { ReactNode } from "react";
-import { FeedbackButton } from "@/components/FeedbackButton/FeedbackButton";
-import { FeatureFlag, isFlagEnabled } from "@/config/FeatureFlagConfig";
 import {
   cssVariablesResolver,
   MODAL_ROOT_Z_INDEX,
@@ -19,9 +17,6 @@ export function AvandarUIProvider({ children }: Props): JSX.Element {
     <MantineProvider theme={Theme} cssVariablesResolver={cssVariablesResolver}>
       <ModalsProvider modalProps={{ zIndex: MODAL_ROOT_Z_INDEX }}>
         <Notifications position="bottom-right" />
-        {isFlagEnabled(FeatureFlag.EnableUserFeedback) ?
-          <FeedbackButton />
-        : null}
         {children}
       </ModalsProvider>
     </MantineProvider>
