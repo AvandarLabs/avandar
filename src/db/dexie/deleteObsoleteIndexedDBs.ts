40b23ab94ec5a79622afefe1ec5fd81f1009f31f
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2026-07-27 21:45:13 -0400

    docs(dexie): document obsolete cleanup retirement

--- src/db/dexie/deleteObsoleteIndexedDBs.ts
+++ src/db/dexie/deleteObsoleteIndexedDBs.ts
@@ -5,7 +5,12 @@ const OBSOLETE_INDEXED_DB_NAMES = [
   "AvandarPlanAnnotationDB",
 ] as const;
 
-/** Deletes obsolete IndexedDB databases that contain retired feature data. */
+/**
+ * Deletes obsolete IndexedDB databases that contain retired feature data.
+ *
+ * Safe to remove after all clients that might retain these databases have
+ * upgraded to Dexie v7.
+ */
 export async function deleteObsoleteIndexedDBs(): Promise<void> {
   await Promise.all(
     OBSOLETE_INDEXED_DB_NAMES.map(async (databaseName) => {
