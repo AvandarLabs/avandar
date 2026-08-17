d66656492ff1a1a73f07c6d60de9d8defb7c6009
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2026-08-17 10:34:53 -0400

    Merge feat/dashcompact
    Made dashboard more compact

--- src/views/DashboardApp/DashboardEditorView/DashboardEditorContent.tsx
+++ src/views/DashboardApp/DashboardEditorView/DashboardEditorContent.tsx
@@ -1,8 +1,12 @@
 import { Flex } from "@mantine/core";
 import { Puck } from "@puckeditor/core";
 import { AppLayout } from "@/components/layouts/AppLayout/AppLayout";
+import { CanvasAgGridStyles } from "@/views/DashboardApp/DashboardEditorView/CanvasAgGridStyles";
 import { DashboardEditorToolbar } from "@/views/DashboardApp/DashboardEditorView/DashboardEditorToolbar";
+import { DASHBOARD_EDITOR_INITIAL_PUCK_UI } from "@/views/DashboardApp/DashboardEditorView/DashboardEditorView.constants";
+import css from "@/views/DashboardApp/DashboardEditorView/DashboardEditorView.module.css";
 import { ShareOnlyAccessAlert } from "@/views/DashboardApp/DashboardEditorView/ShareOnlyAccessAlert";
+import { useFullWidthCanvasViewport } from "@/views/DashboardApp/DashboardEditorView/useFullWidthCanvasViewport/useFullWidthCanvasViewport";
 import { DashboardFilterStateManager } from "@/views/DashboardApp/DashboardFilterStateManager/DashboardFilterStateManager";
 import type { AvaPageData } from "@/views/DashboardApp/AvaPage/AvaPage.types";
 import type { DashboardEditorViewState } from "@/views/DashboardApp/DashboardEditorView/DashboardEditorView";
@@ -22,10 +26,18 @@ export function DashboardEditorContent({
   state,
   workspaceSlug,
 }: Readonly<Props>): ReactElement {
+  const { onAction: onPuckAction, PuckOverride: FullWidthCanvasOverride } =
+    useFullWidthCanvasViewport();
+
   return (
     <DashboardFilterStateManager.Provider>
       <AppLayout floatingToolbar>
-        <Flex direction="column" h="100%" pt={40}>
+        <Flex
+          className={css.compactDashboardEditor}
+          direction="column"
+          h="100%"
+          pt={40}
+        >
           {state.isShareOnlyAccess ?
             <ShareOnlyAccessAlert />
           : null}
@@ -34,11 +46,15 @@ export function DashboardEditorContent({
             metadata={state.metadata}
             config={state.puckConfig}
             height="100%"
+            ui={DASHBOARD_EDITOR_INITIAL_PUCK_UI}
             data={state.editorData ?? state.initialEditorData}
             onChange={(data: Data) => {
               state.dispatch.updateEditorData(data as AvaPageData);
             }}
+            onAction={onPuckAction}
             overrides={{
+              iframe: CanvasAgGridStyles,
+              puck: FullWidthCanvasOverride,
               headerActions: () => {
                 return (
                   <DashboardEditorToolbar
